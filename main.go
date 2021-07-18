c0db723c6b4a567a60c316dc4125e969c24124f1
rishi-org-stack17 <rishi@theflurn.com>
2021-07-18 13:25:10 +0530

    test

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func greet(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello World! %s", time.Now())
+}
 
 func main() {
-	fmt.Println("working")
+	http.HandleFunc("/", greet)
+	http.ListenAndServe(":8080", nil)
 }
